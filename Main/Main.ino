6b8d7814b67a1e2813e98693007ff4fa4261422c
Mugi  Mugendi <mugiendi318@gmail.com>
2023-11-06 11:52:10 +0300

    esp32 code

---
+++ Main/Main.ino
@@ -0,0 +1,24 @@
+#include <WiFi.h>
+
+#define redLedPin 2
+#define yellowLedPin 22
+#define blueLedPin 23
+
+
+const char* ssid     = "iPIC-WIRELESS";
+const char* password = "987654321jica";
+
+
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
